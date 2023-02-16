485e3aec767df11da3b60b84aa682e38a73a841a
Keon <mr.edwards.kj@gmail.com>
2023-02-16 18:55:37 +0000

    update

--- create_vpc.py
+++ create_vpc.py
@@ -7,3 +7,4 @@ vpc = boto3.client('ec2')                   # be sure to call "ec2" and not vpc
 vpc.create_vpc(CidrBlock='10.1.0.0/16')     # CidrBlock is the only field that is required
 
 print("VPC has been created")               # I added for visual confirmation when return is 0
+
