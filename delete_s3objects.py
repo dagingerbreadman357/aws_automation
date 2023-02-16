91a6eb06a61730e9b0e55169bf53ce5a37159cf8
Keon <mr.edwards.kj@gmail.com>
2023-02-16 18:05:31 +0000

    update code to add delete multiple objects

--- delete_s3objects.py
+++ delete_s3objects.py
@@ -3,7 +3,26 @@ import boto3
 s3 = boto3.client('s3')
 
 # Delete single object
-response = s3.delete_object(
-    Bucket='kje-bucket021323',
-    Key='6KwNCresized.png'
-    )
+#s3.delete_object(Bucket='kje-bucket021323', Key='list-script.py'
+#)
+
+#print("Deletion Complete")
+
+
+# Delete multiple objects
+
+import os
+import glob
+
+# list all objects in this bucket in the objects varialble
+
+objects=s3.list_objects(Bucket='kje-bucket021323') ['Contents']
+
+# iterate through items in objects variable for deletion
+
+for object in objects :
+    s3.delete_object(Bucket='kje-bucket021323',
+    Key=object['Key'])
+    
+print("Deletion Complete")
+
