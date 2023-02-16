13274f8c36e34f2b641548349ecd657234c4fc76
Keon <mr.edwards.kj@gmail.com>
2023-02-16 21:09:02 +0000

    big update

---
+++ ec2/create_instance.py
@@ -0,0 +1,16 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+# create ec2 instance with minimum required fields
+
+ec2.run_instances(MaxCount=1,
+        MinCount=1,
+        ImageId="ami-0f1a5f5ada0e7da53",         # us-west-2 ami as of 2/16/23
+        InstanceType="t2.micro",
+#        KeyName="private-ec2",
+#        SecurityGroups=["launch-wizard-6"],
+#        UserData=boot_apache2_script
+)
+
+print("EC2 instance created")
