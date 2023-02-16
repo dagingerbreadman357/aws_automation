fe912cbe3c283bc0ba1ff32d38e574a3b51577ce
Keon <mr.edwards.kj@gmail.com>
2023-02-16 18:08:35 +0000

    added upload multiple files section to code

--- uploadfiles_s3bucket.py
+++ uploadfiles_s3bucket.py
@@ -1,11 +1,30 @@
 import boto3
 
+# how to upload a single file
 
-resource = boto3.client('s3')
+s3 = boto3.client('s3')
 
-resource.upload_file(
+s3.upload_file(
     Filename="/home/ec2-user/environment/redpython/list-script.py",                    # path to the file that need to be uploaded
     Bucket="kje-bucket021323",                                                         # name of the bucket file will be uploaded to
     Key="list-script.py"                                                               # name of file=key  value=content of file
     )
 
+# how to upload multiple files
+
+#import os
+#import glob
+
+#s3 = boto3.client('s3')
+
+#cwd = os.getcwd()
+#cwd = cwd+'/'
+
+#files = glob.glob(cwd + "*.py")
+
+#for file in files:
+#    s3.upload_file(
+#    Filename = file,
+#    Bucket="kje-bucket021323",
+#    Key=file.split("/") [-1]
+#    )
