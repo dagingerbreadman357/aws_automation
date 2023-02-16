120a79d833d8c7f26a51c1211a5f928425b163ad
Keon <mr.edwards.kj@gmail.com>
2023-02-16 19:15:44 +0000

    list of vpc_ids

---
+++ list_vpc_ids.py
@@ -0,0 +1,13 @@
+import boto3
+
+# how to get vpc ID's
+
+vpc = boto3.client('ec2')               # 
+
+response = vpc.describe_vpcs()
+
+vpcs = response['Vpcs']                 # creating Vpcs as the Key for the list
+print(len(vpcs))
+# itertate through VPCS
+for vpc in vpcs:
+    print(vpc['VpcId'])                 # creating VpcId as the value to the key to differentiate each VPC
