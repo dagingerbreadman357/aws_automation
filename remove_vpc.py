4799fb0952f47b9ee4cc5df3dac3d0fde16c1950
Keon <mr.edwards.kj@gmail.com>
2023-02-16 19:36:28 +0000

    updated code

--- remove_vpc.py
+++ remove_vpc.py
@@ -3,13 +3,9 @@ import boto3
 vpc = boto3.client('ec2')
 
 
+delete = vpc.delete_vpc(
+    VpcId='vpc-028a6f09f9646f481'         # "VpcId" is the only mandatory field
+)
 
 
-
-
-
-
-#response = vpc.delete_vpc(
-#    VpcId='string',
-#    DryRun=True|False
-#)
+print('The VPC has been deleted')
